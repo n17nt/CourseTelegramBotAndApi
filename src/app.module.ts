c31417a95d31ebb0dc6135311077f65d49d6a238
Mironshoh <mironshohasadov1603@gmail.com>
2025-05-20 20:07:50 +0500

    merge  from Abubakr

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TelegramModule } from './telegram/telegram.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { CourseModule } from './course/course.module';
 
 @Module({
-  imports: [TelegramModule],
+  imports: [
+    TelegramModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: './src/schem.gql',
+    }),
+    CourseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
